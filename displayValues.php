225aea37ac3ea598354a1b4b5649e235ea8c9c5d
poopi67 <80008191+poopi67@users.noreply.github.com>
2021-03-08 17:19:03 -0500

    Added a leaderboard button
    Will try and handle this function in a prettier way, but right now I don't really care.

--- displayValues.php
+++ displayValues.php
@@ -7,10 +7,32 @@
   <title>PlexDong: <?php  echo $_POST['email']; ?></title>
 </head>
 <body>
+  <?php
+  $email = htmlspecialchars(strtolower($_POST['email']));
+  $db = new DongDB();
+  $db->createDong($db, $email);
+  ?>
+  <body>
+    <br />
+
+    <button type="button" name="button" id="button" onclick="dispLeader();">View Leaderboard</button>
+    <p id="leaderBrd"> </p>
+    <script type="text/javascript">
+    function dispLeader() {
+      let leaderBrd = document.getElementById('leaderBrd');
+      leaderBrd.innerHTML = "<?php $db->displayAll($db);?>";
+      let button = document.getElementById('button');
+      var x = leaderBrd;
+      if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+        button.innerHTML = "Hide Leaderboard";
+      } else {
+        x.style.display = "none";
+        button.innerHTML = "View Leaderboard";
+      }
+    }
+  </script>
+
+
 </body>
 </html>
-<?php
-$email = htmlspecialchars(strtolower($_POST['email']));
-$db = new DongDB();
-$db->createDong($db, $email);
-?>
