35da17035d7c4fa0aafaf4e8e63e3fa5992d0165
ejach <80008191+poopi67@users.noreply.github.com>
2021-06-02 12:50:29 -0400

    Fixed readability/formatting

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 # Handles the query to the API
 def query(user):
     # Takes in the Tautulli API/Server credentials from the .env file
     api_token = os.environ.get('api_token')
     server_url = os.environ.get('server_url')
     user = user
-    api_url_base = '{0}/api/v2?apikey={1}&cmd=get_users_table&order_column=plays&search={2}'.format(server_url, api_token, user)
+    api_url_base = '{0}/api/v2?apikey={1}&cmd=get_users_table&order_column=plays&search={2}'.format(server_url,
+                                                                                                    api_token, user)
     headers = {'Content-Type': 'application/json'}
     response = requests.get(api_url_base, headers=headers)
     users = json.loads(response.text)
@@ -34,8 +36,9 @@ def query(user):
 def index():
     return render_template('index.html')
 
+
 @app.route('/dong', methods=['POST'])
-def displayDong():
+def display_dong():
     email = request.form['email']
     num = query(email)
     # If num doesn't have a value assigned to it, return error
@@ -47,11 +50,10 @@ def displayDong():
         # The variables to create the dong
         # Gets the total watch amount and divides it by 5 then displays the, uh, shaft
         # using the newCount number with "="'s
-        dongDigit = math.trunc(int(num) / 5)
-        dong =  '8' + '='*dongDigit + 'D'
+        dong_digit = math.trunc(int(num) / 5)
+        dong = '8' + '=' * dong_digit + 'D'
         statement = '{0} you have '.format(email) + num + ' plays, therefore '
         return render_template('index.html', statement=statement, dong=dong, title="| " + email)
-        return redirect('/')
 
 
 if __name__ == '__main__':
